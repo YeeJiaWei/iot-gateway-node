d93854e2a0b1d9da7ff694ed9466616a0287ee93
Yee Jia Wei <yeejiawei0811@gmail.com>
2022-11-20 13:01:10 +0800

    refactor examples

--- examples/0_MQTT_Gateway/0_MQTT_Gateway.ino
+++ examples/0_MQTT_Gateway/0_MQTT_Gateway.ino
@@ -1,17 +1,10 @@
-//  FARM DATA RELAY SYSTEM
-//
-//  GATEWAY 2.000
-//
-//  Developed by Timm Bogner (timmbogner@gmail.com) for Sola Gratia Farm in Urbana, Illinois, USA.
-//
-
 #include "fdrs_gateway_config.h"
 #include <fdrs_functions.h>
 
 void setup() {
-beginFDRS();
+  beginFDRS();
 }
 
 void loop() {
-loopFDRS();
+  loopFDRS();
 }
